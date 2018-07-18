87cb66ed5ed226dda4743ceff256a2e45b2210ca
cuijiarui <cuijiarui@gmail.com>
2018-07-18 22:54:43 +0100

    add fragment manager.

--- app/src/main/java/mzx/com/journaler/MainActivity.kt
+++ app/src/main/java/mzx/com/journaler/MainActivity.kt
@@ -1,12 +1,43 @@
 package mzx.com.journaler
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_header.*
+import mzx.com.fragment.ItemsFragment
+import mzx.com.fragment.ManualFragment
+import mzx.com.journaler.R.layout.activity_main
 
-class MainActivity : AppCompatActivity() {
+
+
+class MainActivity : BaseActivity() {
+    //private val tag = "aaaa"
+    override fun getActivityTitle() = R.string.app_name
+    override val tag = "Main activity"
+    override  fun getLayout(): Int {
+        return activity_main
+     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val fragment = ItemsFragment()
+        supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container, fragment)
+                .commit()
+        filter_menu.setText("H")
+        filter_menu.setOnClickListener{
+            val userManualFrg = ManualFragment()
+            supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, userManualFrg)
+                    .addToBackStack("User manual")
+                    .commit()
+        }
     }
+
+
+
+
+
+
+
 }
